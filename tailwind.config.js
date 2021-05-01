ec175bb7caea4e70ece3ab596bda43633cc2e929
Matt Koch <kochm@mskcc.org>
2021-05-01 17:25:22 -0400

    basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        raveblue: {
+          100: '#d6e0eb',
+          200: '#adc2d6',
+          300: '#85a3c2',
+          400: '#5c85ad',
+          500: '#336699',
+          600: '#29527a',
+          700: '#1f3d5c',
+          800: '#14293d',
+          900: '#0a141f'
+        },
+        
+      }
+
+    },
   },
   variants: {
     extend: {},
